357ebf99d0eabecd1482b028b26e167861d26e90
monell <monell.zhong@gmail.com>
2020-03-26 00:09:22 +0800

    bitset range_op

--- cache/include/bitset.h
+++ cache/include/bitset.h
@@ -4,15 +4,22 @@
 #include <cstdlib>
 #include <assert.h>
 
-template<unsigned int CHAR_NUM>
+constexpr unsigned FLOOR_LOG2(unsigned x) {
+    return x == 1? 0: FLOOR_LOG2(x >> 1) + 1;
+}
+constexpr unsigned CEIL_LOG2(unsigned x) {
+    return x == 1? 0: FLOOR_LOG2(x - 1) + 1;
+}
+
+template<unsigned int BIT_NUM>
 class Bitset {
 private:
-    char data[CHAR_NUM];
+    char data[(BIT_NUM + 7) >> 3];
 public:
-    #define CHECK(x) {assert((x) < (CHAR_NUM << 3) && (x) >= 0);}
+    #define CHECK(x) {assert((x) < this->size() && (x) >= 0);}
     Bitset() { memset(data, 0, sizeof(data)); }
-    int size() {
-        return CHAR_NUM << 3;
+    inline int size() {
+        return BIT_NUM;
     }
     void reset(int pos = -1) {
         if (pos < 0) { memset(data, 0, sizeof(data)); }
@@ -31,6 +38,27 @@ public:
     bool test(int pos) {
         CHECK(pos);
         return data[pos >> 3] & (1 << (pos & 0x7));
+    }
+
+    //TODO: better algorithm 
+    void range_set(int s, int e, unsigned int v) {
+        //set [s, e) to v  (from left to right)
+        //v < 2^(e-s)        
+        assert(v < (1 << (e - s)));
+        for (int i = e - 1, j = 1; i >= s; --i, j <<= 1) {
+            if (v & j) set(i);
+            else reset(i);
+        }
+    }
+    unsigned int range_get(int s, int e) {
+        //get value at [s, e)
+        assert(e > s);
+        unsigned int ret = 0;
+        for (int i = s, j = 1; i < e; ++i, j <<= 1) {
+            if (test(i)) ret = (ret << 1) | 1;
+            else ret <<= 1;
+        }
+        return ret;
     }
 };
 
--- cache/test/bitset_test.cc
+++ cache/test/bitset_test.cc
@@ -1,8 +1,25 @@
 #include <gtest/gtest.h>
+#include <stdlib.h>
+#include <iostream>
 #include "bitset.h"
 
+TEST(bitset_test, compile_log) {
+    EXPECT_EQ(CEIL_LOG2(1), 0);
+    EXPECT_EQ(CEIL_LOG2(2), 1);
+    EXPECT_EQ(CEIL_LOG2(3), 2);
+    EXPECT_EQ(CEIL_LOG2(4), 2);
+    EXPECT_EQ(CEIL_LOG2(5), 3);
+    EXPECT_EQ(CEIL_LOG2(6), 3);
+    EXPECT_EQ(CEIL_LOG2(7), 3);
+    EXPECT_EQ(CEIL_LOG2(8), 3);
+    EXPECT_EQ(CEIL_LOG2(9), 4);
+    EXPECT_EQ(CEIL_LOG2(10), 4);
+    EXPECT_EQ(CEIL_LOG2(16), 4);
+    EXPECT_EQ(CEIL_LOG2(17), 5);
+}
 
-TEST(bitset_test, bitset) {
+
+TEST(bitset_test, single_bit_op) {
     Bitset<100> bs;
     for (int i = 0; i < bs.size(); ++i) {
         EXPECT_FALSE(bs.test(i));
@@ -17,5 +34,39 @@ TEST(bitset_test, bitset) {
         EXPECT_TRUE(bs.test(i));
         bs.reset(i);
         EXPECT_FALSE(bs.test(i));
+    }
+}
+
+
+TEST(bitset_test, range_op) {
+    Bitset<100> bs;
+    std::cout << "bs.size() == " << bs.size() << std::endl;
+    bs.range_set(0, 8, 111);
+    EXPECT_EQ(bs.range_get(0, 8), 111);
+
+    bs.range_set(7, 9, 3);
+    EXPECT_EQ(bs.range_get(7, 9), 3);
+
+    bs.range_set(7, 9, 2);
+    EXPECT_EQ(bs.range_get(7, 9), 2);
+
+    bs.range_set(0, 15, 22222);
+    EXPECT_EQ(bs.range_get(0, 15), 22222);
+
+
+    int count = 1000;
+    for (int i = 0; i < count; ++i) {
+        int s = rand() % 100;
+        int e = rand() % 100;
+        if (s == e) continue;
+        if (s > e) {
+            int t = s;
+            s = e;
+            e = t;
+        }
+
+        unsigned int value = rand() % (1 << (e - s));
+        bs.range_set(s, e, value);
+        EXPECT_EQ(bs.range_get(s, e), value);
     }
 }
